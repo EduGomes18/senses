8bf073bc4dce7abc361993599d8a82ec04bd02fc
EduGomes18 <eduardo_gomes1318@outlook.com>
2020-05-30 23:49:03 -0300

    create new page for receive

--- src/Pages/Receive/index.js
+++ src/Pages/Receive/index.js
@@ -1,10 +1,30 @@
 import React from "react";
-import { View } from "react-native";
+import { Red, Primary, Secondary } from "../../Config";
+import {
+  MaterialIcons,
+  MaterialCommunityIcons,
+  Entypo,
+} from "@expo/vector-icons";
 
-import { Container } from "./styles";
+import { Container, CardTitle, Card } from "./styles";
 
 const Receive = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Card color={Red}>
+        <CardTitle>Visual Experience</CardTitle>
+        <MaterialCommunityIcons name="glasses" size={24} color="black" />
+      </Card>
+      <Card color={Secondary}>
+        <CardTitle>Touchable and Tastable</CardTitle>
+        <MaterialIcons name="touch-app" size={24} color="black" />
+      </Card>
+      <Card color={Primary}>
+        <CardTitle>Sound Experience</CardTitle>
+        <Entypo name="sound" size={24} color="black" />
+      </Card>
+    </Container>
+  );
 };
 
 export default Receive;
--- src/Pages/Receive/styles.js
+++ src/Pages/Receive/styles.js
@@ -1,3 +1,24 @@
 import styled from "styled-components/native";
+import Constants from "expo-constants";
+import { Primary, Red } from "../../Config";
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  padding-top: ${Constants.statusBarHeight + "px"};
+  justify-content: space-around;
+  align-items: center;
+`;
+
+export const Card = styled.TouchableOpacity`
+  width: 80%;
+  background:${(props) => props.color}
+  justify-content:center;
+  align-items:center;
+  height: 20%;
+`;
+
+export const CardTitle = styled.Text`
+  font-size: 16px;
+  font-family: Montserrat Regular;
+  color: #fff;
+`;
